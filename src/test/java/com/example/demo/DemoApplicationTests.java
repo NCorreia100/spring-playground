18e8f63cdba8c0500aaefa1ab281ece6171e5f42
Nuno <NCorreia1@my.devry.edu>
2019-04-11 13:49:08 -0400

    added endpoint test

--- src/test/java/com/example/demo/DemoApplicationTests.java
+++ src/test/java/com/example/demo/DemoApplicationTests.java
@@ -2,15 +2,34 @@ package com.example.demo;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.RequestBuilder;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @RunWith(SpringRunner.class)
-@SpringBootTest
+@WebMvcTest(WebController.class)
 public class DemoApplicationTests {
 
+	@Autowired
+	MockMvc mvc;
+
 	@Test
-	public void contextLoads() {
+	public void testHomePage() throws Exception {
+		//set up
+
+		RequestBuilder request = MockMvcRequestBuilders.get("/").param("q","test");
+		//exercise
+		this.mvc.perform(request)
+		//assert
+				.andExpect(status().isOk())
+				.andExpect(content().string("The parameter passed in was test"));
 	}
 
 }
