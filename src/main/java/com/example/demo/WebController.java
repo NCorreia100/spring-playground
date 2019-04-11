6d67f1097f73596f44771878d4526d2dec4f773e
Nuno <NCorreia1@my.devry.edu>
2019-04-11 12:16:28 -0400

    accepting parameters

--- src/main/java/com/example/demo/WebController.java
+++ src/main/java/com/example/demo/WebController.java
@@ -1,13 +1,12 @@
 package com.example.demo;
 
-import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 public class WebController {
 
-    @GetMapping("/")
-    public String getDefault(){
-        return "Jeff and Kay are the best instructors!";
+    @RequestMapping("/")
+    public  String getDefault(@RequestParam("q") String query){
+        return "The parameter passed in was "+query;
     }
 }
